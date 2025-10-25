4a4e91dcbd89545074890fe249d6656ec8699014
br36b <br36b@pm.me>
2025-10-25 12:28:38 -0500

    Add command to store an RSS feed

--- main.go
+++ main.go
@@ -3,8 +3,12 @@ package main
 import (
 	"context"
 	"database/sql"
+	"encoding/xml"
 	"fmt"
+	"html"
+	"io"
 	"log"
+	"net/http"
 	"os"
 	"time"
 
@@ -139,6 +143,113 @@ func handleGetUsers(s *state, cmd command) error {
 	return nil
 }
 
+func handleAggregateFeed(s *state, cmd command) error {
+	// if len(cmd.args) != 1 {
+	// 	return fmt.Errorf("Usage: %s <feed_url>", cmd.name)
+	// }
+	var feedUrl string
+	if len(cmd.args) == 0 {
+		feedUrl = "https://www.wagslane.dev/index.xml"
+	} else {
+		feedUrl = cmd.args[0]
+	}
+
+	feed, err := fetchFeed(context.Background(), feedUrl)
+	if err != nil {
+		return fmt.Errorf("Unable to fetch feed: %w", err)
+	}
+
+	fmt.Printf("Feed: %+v", feed)
+
+	return nil
+}
+
+func handleAddRssFeed(s *state, cmd command) error {
+	if len(cmd.args) != 2 {
+		return fmt.Errorf("Usage: %s <feed_title> <feed_url>", cmd.name)
+	}
+
+	// Get Current User
+	username := s.cfg.CurrentUserName
+	userEntry, err := s.db.GetUser(context.Background(), username)
+	if err != nil {
+		return fmt.Errorf("User '%s' was not found: %v", username, err)
+	}
+
+	// Save Feed
+	feedName, feedUrl := cmd.args[0], cmd.args[1]
+
+	newFeedParams := database.CreateFeedParams{
+		ID:        uuid.New(),
+		CreatedAt: time.Now(),
+		UpdatedAt: time.Now(),
+		Name:      feedName,
+		Url:       feedUrl,
+		UserID:    userEntry.ID,
+	}
+
+	feedEntry, err := s.db.CreateFeed(context.Background(), newFeedParams)
+	if err != nil {
+		return fmt.Errorf("Unable to create feed: %w", err)
+	}
+
+	fmt.Printf("Feed: %+v", feedEntry)
+
+	return nil
+}
+
+type RSSFeed struct {
+	Channel struct {
+		Title       string    `xml:"title"`
+		Link        string    `xml:"link"`
+		Description string    `xml:"description"`
+		Item        []RSSItem `xml:"item"`
+	} `xml:"channel"`
+}
+
+type RSSItem struct {
+	Title       string `xml:"title"`
+	Link        string `xml:"link"`
+	Description string `xml:"description"`
+	PubDate     string `xml:"pubDate"`
+}
+
+func fetchFeed(ctx context.Context, feedUrl string) (*RSSFeed, error) {
+	req, err := http.NewRequestWithContext(ctx, "GET", feedUrl, nil)
+	if err != nil {
+		return &RSSFeed{}, fmt.Errorf("Unable to construct request: %w", err)
+	}
+
+	req.Header.Set("User-Agent", "gator")
+
+	client := &http.Client{}
+	res, err := client.Do(req)
+	if err != nil {
+		return &RSSFeed{}, fmt.Errorf("Unable to perform request: %w", err)
+	}
+	defer res.Body.Close()
+
+	data, err := io.ReadAll(res.Body)
+	if err != nil {
+		return nil, fmt.Errorf("Unable to read response: %w", err)
+	}
+
+	var rssFeedData RSSFeed
+	if err := xml.Unmarshal(data, &rssFeedData); err != nil {
+		return &RSSFeed{}, fmt.Errorf("Unable to unmarshal response body: %w", err)
+	}
+
+	rssFeedData.Channel.Title = html.UnescapeString(rssFeedData.Channel.Title)
+	rssFeedData.Channel.Description = html.UnescapeString(rssFeedData.Channel.Description)
+
+	for i, feedItem := range rssFeedData.Channel.Item {
+		rssFeedData.Channel.Item[i].Title = html.UnescapeString(feedItem.Title)
+		rssFeedData.Channel.Item[i].Description = html.UnescapeString(feedItem.Description)
+	}
+
+	return &rssFeedData, nil
+}
+
 func main() {
 	// Basic app initialization
 	dbConfig, err := config.Read()
@@ -166,6 +277,8 @@ func main() {
 	appCommands.register("register", handlerRegister)
 	appCommands.register("reset", handlerReset)
 	appCommands.register("users", handleGetUsers)
+	appCommands.register("agg", handleAggregateFeed)
+	appCommands.register("addfeed", handleAddRssFeed)
 
 	// Command processing
 	commandArgs := os.Args
